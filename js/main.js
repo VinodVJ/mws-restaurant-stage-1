58166995d719394cb1d2adf49cd70a69d8e4d7c5
Vinod J <vinod@docpulse.com>
2018-06-24 11:09:31 +0530

    style: Made required responsive changes for resturant-list

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoidmlub2QtaiIsImEiOiJjamlmaDJ1ZTkwMjhzM3FueGM3d253dXFhIn0.2fX6Zg8NdjB2tp3Auc14Zw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
