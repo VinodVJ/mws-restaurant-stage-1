39d13be1b07659b471c771f84b7c63f3b334c689
Vinod J <vinod@docpulse.com>
2018-06-28 23:33:45 +0530

    Fix: fixed service worker errors

--- sw.js
+++ sw.js
@@ -1,61 +1,56 @@
-var CACHE_NAME = 'mws-p1-cache-v2';
+var CACHE_NAME = 'mws-p1-cache-v6';
 var urlsToCache = [
   '/',
+  '/css/styles.css',
   '/index.html',
   '/restaurant.html',
-  '/css/styles.css',
   '/js/main.js',
   '/js/restaurant_info.js',
-  '/js/dbhelper.js'
+  '/js/dbhelper.js',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
 ];
 
 self.addEventListener('install', function(event) {
+  console.log("installing Service worker");
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log("Cached Sucessfully");
         return cache.addAll(urlsToCache);
       })
   );
 });
 
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cache_names){
+			return Promise.all(
+				cache_names.filter(function(cache_name){
+					return cache_name.startsWith('mws-') && cache_name!=CACHE_NAME; 
+				}).map(function(cache_name){
+					return cache.delete(cache_name);
+				})
+			)
+		})
+	)
+});
 
 self.addEventListener('fetch', function(event) {
-  const url = new URL(event.request.url);
-  if (url.pathname.startsWith('/restaurant.html')) {
-        event.respondWith(
-            caches.match('restaurant.html')
-            .then(response => response || fetch(event.request))
-        );
-        return;
-  } 
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-                console.log('cache updated')
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-  });
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+        if(response) return response;
+        return fetch(event.request);
+			}
+		)
+	);
+});
