d7c1d51801fd76dae659a88b1ad44b9bfd7c9516
Vinod J <vinod@docpulse.com>
2018-10-19 10:38:40 +0530

    added response clone feature

--- sw.js
+++ sw.js
@@ -48,8 +48,30 @@ self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
 			.then(function(response) {
-        if(response) return response;
-        return fetch(event.request);
+        if(response) {
+          return response;
+        }
+        // return fetch(event.request);
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status != 200 || response.type != 'basic') {
+              return response;
+            }
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
 			}
 		)
 	);
